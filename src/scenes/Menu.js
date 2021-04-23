edf4d399e51f44a3f86dd978b4f4f173ce2fc60b
Ian Richardson <ierichar@ucsc.edu>
2021-04-23 13:13:32 -0700

    early menu and play code

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,14 +4,40 @@ class Menu extends Phaser.Scene {
     }
 
     preload() {
-
+        // load audio and title card
     }
 
     create() {
+        // menu text configuration
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
 
+        // show menu text
+        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - 
+            borderPadding, 'S K A T E R', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2, 'Use SPACEBAR to jump',
+            menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 + borderUISize +
+            borderPadding, 'Press (p) to play!', menuConfig).setOrigin(0.5);
+
+        // define keys
+        keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     }
 
     update() {
-
+        if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
+            // start game
+            this.scene.start('playScene');
+        }
     }
 }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,14 +4,23 @@ class Play extends Phaser.Scene {
     }
 
     preload() {
-
+        // load images/tile sprites
+        this.load.image('floor', './assets/Floor.png');
+        this.load.image('placeholder_char', './assets/Placeholder_Character.png');
+        this.load.image('railing_short', './assets/Railing.png');
+        this.load.image('railing_long,', './assets/Railing2.png');
+        this.load.image('spikes', './assets/Spikes.png');
     }
 
     create() {
+        // define keys
+        keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
+        // initialize score
+        this.playerScore = 0;
     }
 
     update() {
-
+        
     }
 }
