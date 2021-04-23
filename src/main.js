7bf683090367c64fdfe61a607c1c5901fc106d42
Ian Richardson <ierichar@ucsc.edu>
2021-04-23 13:37:52 -0700

    adding prefabs

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ let game = new Phaser.Game(config);
 // set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
-let skaterSpeed = 2;
+let gameSpeed = 2;
 
 // reserve keyboard bindings
 let keySPACE;
---
+++ src/prefabs/Spikes.js
@@ -0,0 +1,9 @@
+class Spike extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+
+        // add object to the existing scene
+        scene.add.existing(this);
+        this.moveSpeed = gameSpeed;
+    }
+}
