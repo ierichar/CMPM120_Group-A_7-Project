00ffb390e5251f1c24a2919e1e7468c2d16e3879
Ian Richardson <ierichar@ucsc.edu>
2021-04-23 12:43:54 -0700

    started main, menu, and play. loaded phaser lib

---
+++ src/main.js
@@ -0,0 +1,23 @@
+/***
+ * Group A_7 - Endless Runner
+ * Authors: Ilda Lara Aguilar, Jackson Bazeal, Ian Richardson
+ * CMPM 120/ARTG 120
+ * Started: April 23rd, 2021
+ */
+
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480,
+    scene: [ Menu, Play ]
+}
+
+let game = new Phaser.Game(config);
+
+// set UI sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
+let skaterSpeed = 2;
+
+// reserve keyboard bindings
+let keySPACE;
