c6ffe71e92cf26f5ca004e83a974d225a0364fab
ilda <67297905+ildxa@users.noreply.github.com>
2021-04-28 18:10:27 -0700

    slug import

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -6,7 +6,7 @@ class Load extends Phaser.Scene {
     preload() {
         // load graphic assets
         this.load.image('floor', './assets/Floor.png');
-        this.load.image('placeholder_char', './assets/Placeholder_Character.png');
+        this.load.image('placeholder_char', './assets/Slug.png');
         this.load.image('starfield', './assets/starfield.png');
         this.load.image('railing_short', './assets/Railing.png');
         this.load.image('railing_long', './assets/Railing2.png');
