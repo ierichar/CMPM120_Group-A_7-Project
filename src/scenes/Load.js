bb0a92bee694371703333a18c1089d4494a9f7e6
ilda <67297905+ildxa@users.noreply.github.com>
2021-04-30 20:38:02 -0700

    load assets

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -7,8 +7,8 @@ class Load extends Phaser.Scene {
         // load graphic assets
         this.load.image('placeholder_char', './assets/SlugSideView.png');
         this.load.image('starfield', './assets/starfield.png');
-        this.load.image('railing_short', './assets/Bench1.png');
-        this.load.image('railing_long', './assets/Bench2.png');
+        this.load.image('bench1', './assets/Bench1.png');
+        this.load.image('bench2', './assets/Bench2.png');
         this.load.image('trashcan', './assets/Trashcan.png');
         this.load.image('spikes', './assets/Spikes.png');
         this.load.image('dirt', './assets/dirt.jpg');
